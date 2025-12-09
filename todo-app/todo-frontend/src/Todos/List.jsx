286b29e8ffc3298e927aec137bb05caacce2b1d7
Tom Raine <tomgraine@gmail.com>
2025-12-09 12:40:36 +0800

    Exercise 12.13 complete, fixed PUT route bug on backend (Exercise 12.7).

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,49 +1,55 @@
-import React from 'react'
-
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
-    deleteTodo(todo)
-  }
+    deleteTodo(todo);
+  };
 
   const onClickComplete = (todo) => () => {
-    completeTodo(todo)
-  }
+    completeTodo(todo);
+  };
 
   return (
     <>
-      {todos.map(todo => {
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-            </span>
-          </>
-        )
+      {todos
+        .map((todo) => {
+          const doneInfo = (
+            <>
+              <span>This todo is done</span>
+              <span>
+                <button onClick={onClickDelete(todo)}> Delete </button>
+                <p>{todo._id}</p>
+              </span>
+            </>
+          );
 
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
+          const notDoneInfo = (
+            <>
+              <span>This todo is not done</span>
+              <span>
+                <button onClick={onClickDelete(todo)}> Delete </button>
+                <button onClick={onClickComplete(todo)}> Set as done </button>
+                <p>{todo._id}</p>
+              </span>
+            </>
+          );
 
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-            <span>
-              {todo.text} 
-            </span>
-            {todo.done ? doneInfo : notDoneInfo}
-          </div>
-        )
-      }).reduce((acc, cur) => [...acc, <hr />, cur], [])}
+          return (
+            <div
+              key={todo._id}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                maxWidth: "70%",
+                margin: "auto",
+              }}
+            >
+              <span>{todo.text}</span>
+              {todo.done ? doneInfo : notDoneInfo}
+            </div>
+          );
+        })
+        .reduce((acc, cur, idx) => [...acc, <hr key={"hr" + idx} />, cur], [])}
     </>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
